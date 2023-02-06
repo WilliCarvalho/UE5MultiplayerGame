0eaec15295d8aa45ded09fbf5ef441fd13062540
Willian Carvalho <willian_matheus14@hotmail.com>
2023-02-06 01:05:14 -0300

    Created Seamless travel, Transition map and Lobby Game mode

---
+++ Source/MPShooterProject/GameMode/LobbyGameMode.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LobbyGameMode.h"
+#include "GameFramework/GameStateBase.h"
+
+//first safest place to access the player controller that joined the game
+void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+
+	//Check How many players we have in the game
+	int32 NumberOfPlayers = GameState.Get()->PlayerArray.Num();
+	if (NumberOfPlayers == 2)
+	{
+		UWorld* World = GetWorld();
+		if (World)
+		{
+			bUseSeamlessTravel = true;
+			World->ServerTravel(FString("/Game/Maps/Map_Game?listen"));
+		}
+	}
+}
